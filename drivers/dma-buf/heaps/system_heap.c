3190afcf30e3e112ebe72dea86e14b22f37903b4
arazy79 <53860204+arazy79@users.noreply.github.com>
2026-08-11 21:04:26 +0100

    Update system_heap.c

--- drivers/dma-buf/heaps/system_heap.c
+++ drivers/dma-buf/heaps/system_heap.c
@@ -25,6 +25,54 @@
 #include "page_pool.h"
 #include "deferred-free-helper.h"
 
+/* Kernel 4.19 compatibility helpers */
+#ifndef for_each_sgtable_sg
+#define for_each_sgtable_sg(sgt, sg, i) \
+	for_each_sg((sgt)->sgl, sg, (sgt)->orig_nents, i)
+#endif
+
+#ifndef for_each_sgtable_page
+#define for_each_sgtable_page(sgt, piter, pgoff) \
+	for_each_sg_page((sgt)->sgl, piter, (sgt)->orig_nents, pgoff)
+#endif
+
+static inline size_t page_size(struct page *page)
+{
+	return PAGE_SIZE << compound_order(page);
+}
+
+static inline int dma_map_sgtable(struct device *dev, struct sg_table *sgt,
+				  enum dma_data_direction dir,
+				  unsigned long attrs)
+{
+	int nents = dma_map_sg_attrs(dev, sgt->sgl, sgt->nents, dir, attrs);
+	if (!nents)
+		return -ENOMEM;
+	sgt->nents = nents;
+	return 0;
+}
+
+static inline void dma_unmap_sgtable(struct device *dev, struct sg_table *sgt,
+				     enum dma_data_direction dir,
+				     unsigned long attrs)
+{
+	dma_unmap_sg_attrs(dev, sgt->sgl, sgt->nents, dir, attrs);
+}
+
+static inline void dma_sync_sgtable_for_cpu(struct device *dev,
+					     struct sg_table *sgt,
+					     enum dma_data_direction dir)
+{
+	dma_sync_sg_for_cpu(dev, sgt->sgl, sgt->nents, dir);
+}
+
+static inline void dma_sync_sgtable_for_device(struct device *dev,
+						struct sg_table *sgt,
+						enum dma_data_direction dir)
+{
+	dma_sync_sg_for_device(dev, sgt->sgl, sgt->nents, dir);
+}
+
 static struct dma_heap *sys_heap;
 static struct dma_heap *sys_uncached_heap;
 
